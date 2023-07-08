51f997c3bc925553ad0878428215bcd52afde007
SafiaJotey <safiaahmed1786@gmail.com>
2023-07-09 03:16:01 +0600

    [ADD] total calculatin, load data in checkout page

--- src/redux/feature/cart/cartSlice.ts
+++ src/redux/feature/cart/cartSlice.ts
@@ -5,9 +5,11 @@ import { createSlice } from '@reduxjs/toolkit';
 
 interface Icart {
   products: IProduct[];
+  total: number;
 }
 const initialState: Icart = {
   products: [],
+  total: 0,
 };
 const cartSlice = createSlice({
   name: 'cart',
@@ -22,6 +24,7 @@ const cartSlice = createSlice({
       } else {
         state.products.push({ ...action.payload, quantity: 1 });
       }
+      state.total += action.payload.price;
     },
     removeOneFromCart: (state, action: PayloadAction<IProduct>) => {
       const existing = state.products.find(
@@ -34,12 +37,14 @@ const cartSlice = createSlice({
           (product) => product._id !== action.payload._id
         );
       }
+      state.total -= action.payload.price;
     },
 
     removeFromCart: (state, action: PayloadAction<IProduct>) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload._id
       );
+      state.total -= action.payload.price * action.payload.quantity!;
     },
   },
 });
